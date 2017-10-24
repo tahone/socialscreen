6ec1c978ee40dc213ec0798868c9e1fa06513d49
Tomi Ahonen <tomi.ahonen@cinia.fi>
2017-10-24 15:40:30 +0300

    Refactor Weather

--- src/Views/Bricks/Weather/index.jsx
+++ src/Views/Bricks/Weather/index.jsx
@@ -6,23 +6,26 @@ import _get from 'lodash/get';
 
 import weatherIcons from 'weather-icons/css/weather-icons.css';
 
-import BaseBrick from '../../BaseBrick';
-
-const apiKey = '';
+import BaseBrick from '../';
 
 const lat = 62.237198;
 const lon = 25.742182;
 
 @observer
 export default class Weather extends BaseBrick {
+    static defaultProps = {
+        lat,
+        lon
+    };
 
     @observable data = {};
 
     componentWillMount() {
+        const {lat, lon, apiKey} = this.props;
         const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
         fetch(url).then((response) => {
             response.json().then(action((json) => {
-                console.log('Weather data', json);
+                console.log('Weather data', json, weatherIcons);
                 this.data = json;
             }))
         }).catch(console.log);
@@ -30,10 +33,19 @@ export default class Weather extends BaseBrick {
 
     render() {
         const classes = `${weatherIcons.wi} ${weatherIcons['wi-day-sunny']}`;
+        const thermometer = `${weatherIcons.wi} ${weatherIcons['wi-thermometer']}`;
+        const humidity = `${weatherIcons.wi} ${weatherIcons['wi-humidity']}`;
         return (
             <div style={{fontSize: '72px', textAlign: 'center'}}>
                 <i className={classes}/>
-                <div>{_get(this.data,'main.temp', 273.15) - 273.15} &deg;C</div>
+                <div>
+                    <i className={thermometer}/>
+                    {_get(this.data,'main.temp', 273.15) - 273.15} &deg;C
+                </div>
+                <div>
+                    <i className={humidity}/>
+                    {_get(this.data,'main.humidity', 0)} %
+                </div>
             </div>
         );
     }
